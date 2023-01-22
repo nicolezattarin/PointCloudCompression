be0f2d2f24be6ad435b94d6ac551211b1bcb362a
Nicole Zattarin <72666452+nicolezattarin@users.noreply.github.com>
2023-01-22 23:22:01 +0100

    Delete run_all_bis.sh

--- PointNetAutoencoder/run_all_bis.sh
+++
@@ -1,6 +0,0 @@
-for class_index in 9 10 100
-do
-    root_dir="dataset_autoencoder_labels/label_"$class_index
-    save_dir="checkpoints/label_"$class_index
-    python3.9 train.py --root $root_dir --batchsize 32 --epoches 20 --saved_path $save_dir --lr 0.0005
-done
